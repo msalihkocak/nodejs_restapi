403bd2d9eddf23f0b42486a8addb2f6895a6e718
Mehmet Salih Koçak <kocaksalihm@gmail.com>
2018-11-08 00:25:57 +0300

    Serve public HTML files and receive post requests

--- app.js
+++ app.js
@@ -1,20 +1,42 @@
-// load our app server using express somehow..
+
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
+app.use(express.static("./public"))
 app.use(morgan('combined'))
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.get("/", (req, res) => {
+  console.log("Responding to root route")
+  res.send("Hello from root route.")
+})
+
+app.get("/users", (req, res) => {
+  const connection = getConnection()
+
+  const queryString = "SELECT * FROM users"
+  connection.query(queryString, (err, rows, fields) => {
+    if (err){
+      console.log("Failed to fetch users: " + err)
+      res.sendStatus(500)
+      return
+    }
+
+    console.log("Users fetched successfully. ")
+    const users = rows.map((row) => {
+      return {firstName:row.first_name, lastName:row.last_name}
+    })
+    res.json(users)
+  })
+})
 
 app.get('/users/:id', (req, res) => {
   console.log("Fetching user with id: " + req.params.id)
 
-  const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'passwd',
-    database: 'salih_mysql'
-  })
+  const connection = getConnection()
 
   const userId = req.params.id
   console.log(userId)
@@ -32,22 +54,34 @@ app.get('/users/:id', (req, res) => {
     })
     res.json(users)
   })
-
-  // res.end()
 })
 
-app.get("/", (req, res) => {
-  console.log("Responding to root route")
-  res.send("Hello from root rootue.")
+app.post("/user_create", (req, res) =>{
+  console.log("Trying to create user")
+  
+  const firstName = req.body.create_first_name
+  const lastName = req.body.create_last_name
+  const connection = getConnection()
+
+  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?,?)"
+  connection.query(queryString, [firstName,lastName], (err, rows, fields) => {
+    if(err){
+      console.log(err);
+      res.sendStatus(500)
+      return
+    }
+    res.json({isSuccess:true})
+  })
 })
 
-app.get("/users", (req, res) => {
-  var user1 = {firstName: "Mehmet Salih", lastName: "Koçak"}
-  const user2 = {firstName: "Safiye", lastName: "Koçak"}
-  res.json([user1, user2])
-})
-
-
+function getConnection(){
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'passwd',
+    database: 'salih_mysql'
+  })
+}
 
 app.listen(3003, () => {
   console.log("Server is up and listening on port 3003...");
